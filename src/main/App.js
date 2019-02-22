c94c4c4e19ac6039483be4b2daa6dcc2dc90213d
PhuongNgo <ngohoai.phuong@gmail.com>
2019-02-22 14:19:04 +0700

    add home page, bootstrap, define basic route

--- src/main/App.js
+++ src/main/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import MainRoute from '../routers/main-router';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <header className="App-header">
         </header>
         <div className="App-container">
+          <MainRoute/>
         </div>
         <footer className="App-footer">
         </footer>
