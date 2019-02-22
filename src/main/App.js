8a9e46a0114c9824ca7290281b6d8b1ad3fa7a8f
PhuongNgo <ngohoai.phuong@gmail.com>
2019-02-22 13:41:31 +0700

    edit app

---
+++ src/main/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <div className="App-container">
+        </div>
+        <footer className="App-footer">
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
