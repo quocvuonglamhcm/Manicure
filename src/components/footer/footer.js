47334f2fd4b7ab242ad80af0cc20dddac1ef172b
Trần Minh Trí <tmtzminhtri@gmail.com>
2019-02-24 18:12:13 +0700

    fix layout footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
 import './footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGooglePlay, faAppStoreIos  } from '@fortawesome/free-brands-svg-icons'
+import { faSkype, faFacebook, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faMapMarkerAlt, faMobileAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+import img1 from '../../assets/images/appstore.svg';
+import img2 from '../../assets/images/chplay.svg';
 
 export default function FooterComponent() {
-    const ic_appStore = <FontAwesomeIcon className="fontSize25 mr-2" icon={faAppStoreIos} />
-    const ic_googlePlay = <FontAwesomeIcon className="fontSize25 mr-2" icon={faGooglePlay} />
+    const ic_facebook = <FontAwesomeIcon className="fontSize25 white " icon={faFacebook} />
+    const ic_skype = <FontAwesomeIcon className="fontSize25 white" icon={faSkype} />
+    const ic_twitter = <FontAwesomeIcon className="fontSize25 white" icon={faTwitter} />
+    const ic_linkedin = <FontAwesomeIcon className="fontSize25 white" icon={faLinkedin} />
+    const ic_mapMaker = <FontAwesomeIcon className="fontSize25 mr-2" icon={faMapMarkerAlt} />
+    const ic_mobileAlt = <FontAwesomeIcon className="fontSize25 mr-2" icon={faMobileAlt} />
+    const ic_envelope = <FontAwesomeIcon className="fontSize25 mr-2" icon={faEnvelope} />
     return (
         <div className="footer">
             <Container>
@@ -35,24 +44,55 @@ export default function FooterComponent() {
                         </form>
                     </Col>
                     <Col md={3}>
-                        <div className="download font-weight-bold">Tải App</div>
-                        <div className="chPlay">{ic_googlePlay}</div>
-                        <div className="appStore">{ic_appStore}</div>
+                        <div className="download font-weight-bold pb-2">Tải App</div>
+                        <div className="chPlay pb-1">
+                            <img src={img1} alt="ChPlay" />
+                        </div>
+                        <div className="appStore">
+                            <img src={img2} alt="Appstore" />
+                        </div>
                     </Col>
                     <Col md={3}>
                         <div className="contactTitle font-weight-bold pb-2">Thông tin liên hệ</div>
-                        <div className="address pb-1">38 Nail Street, New York, USA</div>
-                        <div className="phoneNumber pb-1">+84 3123 9123</div>
-                        <div className="supportMail">nail.system.2019@gmail.com</div>
-                        <div className="footSocial"></div>
+                        <div className="address pb-1">
+                           {ic_mapMaker} 38 Nail Street, New York, USA
+                        </div>
+                        <div className="phoneNumber pb-1">
+                            {ic_mobileAlt} +84 3123 9123
+                        </div>
+                        <div className="supportMail">
+                            {ic_envelope} nail.system.2019@gmail.com
+                        </div>
+                        <Row className="footSocial">
+                            <Col sm={3}>
+                                <a href="https://www.twitter.com" rel="noopener noreferrer">
+                                    {ic_twitter}
+                                </a>
+                            </Col>
+                            <div className="col-sm-3">
+                                <a href="https://www.linkedin.com" rel="noopener noreferrer">
+                                    {ic_linkedin}
+                                </a>
+                            </div>
+                            <div className="col-sm-3">
+                                <a href="https://www.facebook.com" rel="noopener noreferrer">
+                                    {ic_facebook}
+                                </a>
+                            </div>
+                            <div className="col-sm-3">
+                                <a href="https://www.skype.com" rel="noopener noreferrer">
+                                    {ic_skype}
+                                </a>
+                            </div>
+                        </Row>
                     </Col>
                 </Row>
-                <div className="footBar">
-                    <Container>
-                        <div className="copywright">2019 Copywright by Nails-system Corp</div>
-                    </Container>
-                </div>
             </Container>
+            <div className="footBar">
+                <Container>
+                    <div className="copywright">2019 Copywright by Nails-system Corp</div>
+                </Container>
+            </div>
         </div>
     )
 }
