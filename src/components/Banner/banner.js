4d16deabd4e60ffa2ef52deafca3d5690065f126
Trong Anh <tronganhnguyen222@gmail.com>
2019-02-24 17:54:54 +0700

    background

---
+++ src/components/Banner/banner.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class Background extends Component {
+  render() {
+    return (
+      <div className="App">
+         <img src="https://uphinhnhanh.com/images/2019/02/24/bg20ffc9e9986b2b11.png" alt="bg20ffc9e9986b2b11.png" border="0" />
+          <p>đăng nhập hoặc đăng ký ngay</p>
+          <button className="btnSignup">Đăng ký</button>
+          <button className="btnLogin">Đăng nhập</button>
+      </div>
+    );
+  }
+}
+
+export default Background;
--- src/main/App.js
+++ src/main/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import MainRoute from '../routers/main-router';
-
+import Background from '../components/Banner/banner.js';
+import '../components/Banner/banner.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+            <Background/>
         </header>
         <div className="App-container">
           <MainRoute/>
