771984f04195fd15e95b965515b16ed742ea6e37
quocvuonglamhcm <quocvuonglamhcm@gmail.com>
2019-03-05 21:35:42 +0700

    add phone

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 import { Col, Button } from 'react-bootstrap';
+import fireBase from '../../js/firebase';
 
 import './login.css'
 
 export default class Login extends Component {
-    Continue = () =>{
-        this.props.nextStep()
+    constructor(props) {
+        super(props);
+        this.state = {
+            phone: ''
+        }
     }
+
+    Continue = () =>{
+        const data = {
+                phone_number: this.state.phone,
+                verify_number: Math.random().toString().substr(2, 4),
+                status: false,
+                created_at: Date.now()
+        };
+
+        fireBase.database().ref(`nails/signup/${this.state.phone}`)
+            .set(data)
+            .then(function(){
+                console.log('ok');
+            })
+            .catch(function(){
+                console.log('no');
+            });
+        }
+
+    handleOnChange = (e) => {
+        console.log(e.target.name, e.target.value);
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render() {
         return (
             <Col sm={12}>
                 <b style={{ userSelect: 'none' }}>SĐT</b>
                 <input type='text' readOnly value='Viet Nam (+84)' className='mavung' />
-                <input type='text' placeholder='Nhập số điện thoại' className='mavung mr-3' autoFocus />
+                <input
+                    type='text' placeholder='Nhập số điện thoại'
+                    name='phone' onChange={this.handleOnChange}
+                    className='mavung mr-3' autoFocus />
                 <Button variant="warning" onClick = {this.Continue}>Gửi mã xác nhận</Button>
             </Col>
         )
