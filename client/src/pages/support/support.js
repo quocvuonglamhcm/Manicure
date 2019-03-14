9d382c61350adeb8251640036086dbe672a22fa3
quocvuonglamhcm <quocvuonglamhcm@gmail.com>
2019-03-14 21:25:00 +0700

    edit

--- client/src/pages/support/support.js
+++ client/src/pages/support/support.js
@@ -1,6 +1,32 @@
 import React, { Component } from 'react';
 
 class SupportPage extends Component {
+	state = {
+		nameService = [
+			{
+				name:'đặt lịch',
+				image:''
+			},
+				{
+				name:'kỹ thuật',
+				image:''
+			},
+				{
+				name:'tài khoản',
+				image:''
+			},
+				{
+				name:'từ người thợ',
+				image:''
+			},
+		],
+		question=[
+			'Tại sao tôi chỉ có 30 phút để đặt mua vé ?',
+			'Tôi có được đặt lịch với nhiều thợ trong cùng một đơn giao dịch không ?',
+			'Tôi có được hủy lịch không ?'
+		]
+	}
+
   render() {
     return (
     	<h1>Trang Ho tro</h1>
