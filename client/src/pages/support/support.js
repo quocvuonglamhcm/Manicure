3c94bcdb86a3ca09e00ee50a8abc613a0c1bd1c9
quocvuonglamhcm <quocvuonglamhcm@gmail.com>
2019-03-16 20:37:35 +0700

    e

--- client/src/pages/support/support.js
+++ client/src/pages/support/support.js
@@ -1,35 +1,47 @@
 import React, { Component } from 'react';
 
 class SupportPage extends Component {
-	state = {
-		nameService:[
-			{
-				name:'đặt lịch',
-				image:''
-			},
-				{
-				name:'kỹ thuật',
-				image:''
-			},
-				{
-				name:'tài khoản',
-				image:''
-			},
-				{
-				name:'từ người thợ',
-				image:''
-			},
-		],
-		question:[
-			'Tại sao tôi chỉ có 30 phút để đặt mua vé ?',
-			'Tôi có được đặt lịch với nhiều thợ trong cùng một đơn giao dịch không ?',
-			'Tôi có được hủy lịch không ?'
-		]
+	constructor(props){
+		super(props);
+		this.state = {
+			 nameService: [
+        {
+          name: "đặt lịch",
+          image: ""
+        },
+        {
+          name: "kỹ thuật",
+          image: ""
+        },
+        {
+          name: "tài khoản",
+          image: ""
+        },
+        {
+          name: "từ người thợ",
+          image: ""
+        }
+      ]
+		}
 	}
+	// state = {
+	// 	,
+	// 	question:[
+	// 		'Tại sao tôi chỉ có 30 phút để đặt mua vé ?',
+	// 		'Tôi có được đặt lịch với nhiều thợ trong cùng một đơn giao dịch không ?',
+	// 		'Tôi có được hủy lịch không ?'
+	// 	]
+	// }
 
   render() {
+  	var element = this.state.nameService.map((item ) => {
+  		return <h1 key ={item+Math.random()}>{ item.name }</h1>
+  	})
+
     return (
-    	<h1>Trang Ho tro</h1>
+    	<div>
+    		{ element }
+    	</div>
     );
   }
 }
