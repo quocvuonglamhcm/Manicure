b4d691df6fc08034f3087792d2ec00d8b577d8fe
Giang Tu <vantudie@gmail.com>
2019-03-17 11:10:16 +0700

    capnhatthongtinkhachhang

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './home.css';
 import Banner from '../../components/Banner/banner'
 import Bodyloiich from '../../components/body-loi-ich/body-loi-ich'
+import CapnhatthongtinkhachhangComponent from '../../components/cap-nhat-thong-tin-khach-hang/cap-nhat-thong-tin-khach-hang'
 const HomePage = () => (
 	<div>
 		<Banner />
@@ -9,6 +10,10 @@ const HomePage = () => (
 			<div className='register-title'>
 				<div className='body-loi-ich'>
 						<Bodyloiich/>
+				</div>
+
+				<div className='cap-nhat-thong-tin-khach-hang'>
+					<CapnhatthongtinkhachhangComponent />
 				</div>
 			</div>
 		</div>
