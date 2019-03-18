e0f55d3dba355551e8ad6b0403f377c4b3d2f5f1
Trong Anh <tronganhnguyen222@gmail.com>
2019-03-18 09:38:54 +0700

    update script for app, create doc for api

--- client/src/components/body-loi-ich/body-loi-ich.js
+++ client/src/components/body-loi-ich/body-loi-ich.js
@@ -2,35 +2,33 @@ import React, { Component } from 'react';
 import './body-loi-ich.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
-import { Row,Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 
 export default function bodyloiichComponent() {
-    const ic_check = <FontAwesomeIcon className="float-left color-red" icon={faCheck} />
+    const ic_check = <FontAwesomeIcon className="IconCheck" icon={faCheck} />
     return (
         <div className='body-loi-ich mt-3'>
             <div className='text-tieu-de'>Nails Partner sẽ mang đến cho </div>
-
             <div id='card-1'>
-                <div className='text-tieu-de text-align-left'>Khách hàng </div>
+                <div className='text-tieu-de '>Khách hàng </div>
                 <Row>
-                    {ic_check} 
-                    <div className='contentloiich'>Làm đẹp tại mọi lúc, mọi nơi</div></Row>
+                    {ic_check}
+                    <div className='ContentLoiIch'>Làm đẹp tại mọi lúc, mọi nơi</div></Row>
                 <Row>
-                {ic_check} 
-                <div className='contentloiich'>Luôn cập nhật xu hướng làm đẹp</div></Row>
+                    {ic_check}
+                    <div className='ContentLoiIch'>Nhanh chóng cập nhật xu hướng làm đẹp mới nhất</div></Row>
                 <Row>
-                {ic_check} 
-                <div className='contentloiich'>Thuận tiện chia sẻ kiến thức làm đẹp</div></Row>
+                    {ic_check}
+                    <div className='ContentLoiIch'>Thuận tiện trao đổi với mọi người về những tác phẩm đẹp</div></Row>
             </div>
-
             <div id='card-2'>
-                <div className='text-tieu-de text-align-left'>Thợ Nails </div>
-                <Row>{ic_check} 
-                <div className='contentloiich'>Dễ dàng tiếp cận các khách hàng mới</div></Row>
+                <div className='text-tieu-de '>Thợ Nails </div>
                 <Row>{ic_check}
-                <div className='contentloiich'>Giới thiệu bản thân miễn phí</div></Row>
-                <Row>{ic_check} 
-                <div className='contentloiich'>Chia sẻ kiến thức làm đẹp với mọi người</div></Row>
+                    <div className='ContentLoiIch'>Dễ dàng tiếp cận các khách hàng mới</div></Row>
+                <Row>{ic_check}
+                    <div className='ContentLoiIch'>Giới thiệu bản thân miễn phí</div></Row>
+                <Row>{ic_check}
+                    <div className='ContentLoiIch'>Thoải mái chia sẻ kinh nghiệm làm đẹp với mọi người</div></Row>
             </div>
         </div>)
 }
--- server/apis/http_code.js
+++ server/apis/http_code.js
@@ -1,5 +1,39 @@
 const express = require('express')
 
 var httpCode = {
+    authStatus: function(status) {
+        let jsonData = {}
+
+        switch (status) {
+          case 'auth/user-not-found':
+            return { 
+                statusCode: 500, 
+                message: 'User not found' 
+            }
+
+          case 'auth/wrong-password':
+            return {
+                statusCode: 401, 
+                message: 'Wrong password' 
+            }
+          case 'auth/invalid-email':
+            return {
+                statusCode: 402, 
+                message: 'Invalid email' 
+            }
+          case 'auth/user-disabled':
+              return {
+                statusCode: 403, 
+                message: 'Account disable' 
+            }
+        }
+        
+        return {
+            statusCode: 200,
+            message: ''
+        }
     
+    }
 }
+
+module.exports = httpCode;
