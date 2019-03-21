2c84d17567c611218abbb21842110c087bd86b3e
Angel <superangel.lucky@gmail.com>
2019-03-21 23:59:29 +0700

    Ahaha

---
+++ client/src/components/nailer-id/nailer-social-right.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Row, Col, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+class NailerSocialRight extends Component {
+    render() {
+        return (
+            
+                <div className='nailer-box-infor-right box mt-3 pt-3'>
+                    <div className='box float-left col-sm-6 center'>
+                        <Link to={'/nailer-new'}>Sản phẩm gần đây</Link>
+                    </div>
+                    <div className='box float-left col-sm-6 center'>
+                        <Link to={'/nailer-social'}>Đóng góp cho cộng đồng</Link>
+                    </div>
+                    <h2>Dong gop cho cong dong</h2>
+                </div>
+                
+            
+        );
+    }
+}
+
+export default NailerSocialRight;
--- client/src/pages/nailer/nailer.js
+++ client/src/pages/nailer/nailer.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import { Row, Col, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './nailer.css'
+import NailerID from '../../components/nailer-id/nailer-id'
+import NailerFilterRate from '../../components/nailer-id/nailer-filter-rate'
+import NailerFilterLocation from '../../components/nailer-id/nailer-filter-location'
+
 class NailerPage extends Component {
   render() {
     return (
-      <h1>Trang tho Nail</h1>
+      <Container>
+        <Row>
+          <NailerFilterRate />
+          <NailerFilterLocation />
+        </Row>
+        <Row>
+          <Col sm={2} md={2} sx={2}>
+            <Link to={'/nailer-new'}><NailerID /></Link>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={2} md={2} sx={2} className='center mt-3 mb-3'>
+            <button type="button" class="btn btn-primary btn-block">Xem thêm </button>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
