752801f1e99eb06ba15e9f1d2bba4bd8464b9450
Aiz <kaage.azusa@gmail.com>
2019-03-21 21:17:25 +0700

    Thay oi!

--- client/src/pages/support/support.js
+++ client/src/pages/support/support.js
@@ -6,6 +6,7 @@ import InputGroup from 'react-bootstrap/InputGroup'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
+// import ic_search_bar from '../../assets/images/ic_global_search_defaul.svg'
 import datlich from '../../assets/images/ic_datlich.svg';
 import kythuat from '../../assets/images/ic_.svg';
 import taikhoan from '../../assets/images/ic_taikhoan.svg';
@@ -38,65 +39,69 @@ class SupportPage extends Component {
 	// }
 
 	render() {
-		let ic_search = <FontAwesomeIcon className="fontSize25 white " icon={faSearch} />
+		let ic_search = <FontAwesomeIcon className="ic-search " icon={faSearch} />
 		return (
-			<div id="sp-main">
-				<div className="sp-search-bar">
-					<div className="par">
-						Xin chào, Nail Partner có thể giúp gì cho bạn?
+				<div id="sp-main">
+					<div className="sp-search-bar">
+						<div className="par">
+							Xin chào, Nail Partner có thể giúp gì cho bạn?
 					</div>
 
-					<InputGroup className="mb-3 sp-input" >
-						<Form.Control
-							placeholder="Bạn thắc mắc gì về Nail Partner?"
-						/>
-						<InputGroup.Append>
-							<Button variant="sp-ic_search">{ic_search}</Button>
-						</InputGroup.Append>
-					</InputGroup>
+						<div className="sp-input-form">
+							<input type="search" className="input-text" placeholder="Bạn có thắc mắc gì về Nail Partner" />
+							<button type="button" className="sp-button-search ic_search">
+								{ic_search}
+							</button>
+						</div>
+					</div>
+
+					<div className="sp-menu" >
+						<ul className="sp-menu_ul">
+							<li>
+								<div className="images">
+									<img src={datlich} />
+								</div>
+								<div className="mn_text">
+									Đặt lịch
+							</div>
+							</li>
+
+							<li>
+								<div className="images">
+									<img src={kythuat} />
+								</div>
+								<div className="mn_text">
+									Kỹ thuật
+							</div>
+							</li>
+
+							<li>
+								<div className="images">
+									<img src={taikhoan} />
+								</div>
+								<div className="mn_text">
+									Tài khoản
+							</div>
+							</li>
+
+							<li>
+								<div className="images">
+									<img src={tho} />
+								</div>
+								<div className="mn_text">
+									Từ người thợ
+							</div>
+							</li>
+
+						</ul>
+					</div>
+
+					
+					<div className="sp-chatbox sp-contact">
+					<div className="sp-contact-slo">Liên hệ với chúng tôi!</div>
+						<button type="button" className="sp-contact-button">Trò chuyện ngay</button>
+					</div>
 				</div>
-
-				<div className="sp-menu" >
-					<ul className="sp-menu_ul">
-						<li>
-							<div className="images">
-								<img src={datlich} />
-							</div>
-							<div className="mn_text">
-								Đặt lịch
-							</div>
-						</li>
-
-						<li>
-							<div className="images">
-								<img src={kythuat} />
-							</div>
-							<div className="mn_text">
-								Kỹ thuật
-							</div>
-						</li>
-
-						<li>
-							<div className="images">
-								<img src={taikhoan} />
-							</div>
-							<div className="mn_text">
-								Tài khoản
-							</div>
-						</li>
-
-						<li>
-							<div className="images">
-								<img src={tho} />
-							</div>
-							<div className="mn_text">
-								Từ người thợ
-							</div>
-						</li>
-
-					</ul>
-				</div>
-			</div>
 		);
 	}
 }
