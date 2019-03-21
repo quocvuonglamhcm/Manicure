7b9d0181e2e5ce1f3fb10a3b3cfc656f95c934f2
Angel <superangel.lucky@gmail.com>
2019-03-22 00:15:52 +0700

    Hello

--- client/src/pages/support/support.js
+++ client/src/pages/support/support.js
@@ -41,67 +41,67 @@ class SupportPage extends Component {
 	render() {
 		let ic_search = <FontAwesomeIcon className="ic-search " icon={faSearch} />
 		return (
-				<div id="sp-main">
-					<div className="sp-search-bar">
-						<div className="par">
-							Xin chào, Nail Partner có thể giúp gì cho bạn?
+			<div id="sp-main">
+				<div className="sp-search-bar">
+					<div className="par">
+						Xin chào, Nail Partner có thể giúp gì cho bạn?
 					</div>
 
-						<div className="sp-input-form">
-							<input type="search" className="input-text" placeholder="Bạn có thắc mắc gì về Nail Partner" />
-							<button type="button" className="sp-button-search ic_search">
-								{ic_search}
-							</button>
-						</div>
-					</div>
-
-					<div className="sp-menu" >
-						<ul className="sp-menu_ul">
-							<li>
-								<div className="images">
-									<img src={datlich} />
-								</div>
-								<div className="mn_text">
-									Đặt lịch
-							</div>
-							</li>
-
-							<li>
-								<div className="images">
-									<img src={kythuat} />
-								</div>
-								<div className="mn_text">
-									Kỹ thuật
-							</div>
-							</li>
-
-							<li>
-								<div className="images">
-									<img src={taikhoan} />
-								</div>
-								<div className="mn_text">
-									Tài khoản
-							</div>
-							</li>
-
-							<li>
-								<div className="images">
-									<img src={tho} />
-								</div>
-								<div className="mn_text">
-									Từ người thợ
-							</div>
-							</li>
-
-						</ul>
-					</div>
-
-					
-					<div className="sp-chatbox sp-contact">
-					<div className="sp-contact-slo">Liên hệ với chúng tôi!</div>
-						<button type="button" className="sp-contact-button">Trò chuyện ngay</button>
+					<div className="sp-input-form">
+						<input type="search" className="input-text" placeholder="Bạn có thắc mắc gì về Nail Partner" />
+						<button type="button" className="sp-button-search ic_search">
+							{ic_search}
+						</button>
 					</div>
 				</div>
+
+				<div className="sp-menu" >
+					<ul className="sp-menu_ul">
+						<li>
+							<div className="images">
+								<img src={datlich} />
+							</div>
+							<div className="mn_text">
+								Đặt lịch
+							</div>
+						</li>
+
+						<li>
+							<div className="images">
+								<img src={kythuat} />
+							</div>
+							<div className="mn_text">
+								Kỹ thuật
+							</div>
+						</li>
+
+						<li>
+							<div className="images">
+								<img src={taikhoan} />
+							</div>
+							<div className="mn_text">
+								Tài khoản
+							</div>
+						</li>
+
+						<li>
+							<div className="images">
+								<img src={tho} />
+							</div>
+							<div className="mn_text">
+								Từ người thợ
+							</div>
+						</li>
+
+					</ul>
+				</div>
+
+
+				<div className="sp-chatbox sp-contact">
+					<div className="sp-contact-slo">Liên hệ với chúng tôi!</div>
+					<button type="button" className="sp-contact-button">Trò chuyện ngay</button>
+				</div>
+			</div>
 		);
 	}
 }
