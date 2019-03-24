0fee0073d3b26dbb6070323c30afecbd48dbba65
Giang tu <vantudie@gmail.com>
2019-03-24 22:49:30 +0700

    trang-cap-nhat-thong-tin-khach-hang

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import './home.css';
 import Banner from '../../components/Banner/banner'
-import ProfileUserdetail from '../../components/profileuserdetail/profileuserdetail';
 const HomePage = () => (
 	<div>
 		<Banner />
 		<div className='container'>
 			<div className='register-title'>
-			</div>
-			<div className='body-loi-ich'>
-				<ProfileUserdetail />
 			</div>
 		</div>
 	</div>
--- client/src/routers/routers.js
+++ client/src/routers/routers.js
@@ -8,14 +8,11 @@ import ServicePage from '../pages/service/service';
 import ProductPage from '../pages/product/product';
 import NailerPage from '../pages/nailer/nailer';
 import SupportPage from '../pages/support/support';
-import NailerPageNew from '../pages/nailer/nailer-new';
-import NailerPageSocial from '../pages/nailer/nailer-social.js';
-<<<<<<< HEAD
+// import NailerPageNew from '../pages/nailer/nailer-new';
+// import NailerPageSocial from '../pages/nailer/nailer-social.js';
+// import Banggiadichvu from '../pages/banggiadichsvu/banggiadichvu';
 import ProfileUser from '../pages/profileuser/profileuser';
-=======
-import FormSignInComponent from '../components/SignIn/SignIn';
-import Bangiadichvu from '../pages/banggiadichsvu/banggiadichvu';
->>>>>>> 503a9f7f74c5d4cf2e9fdc8b182ceade39484d71
+
 //import ModalDialog from '../components/modal/modal';
 
 const MainRoute = () => (
@@ -30,14 +27,12 @@ const MainRoute = () => (
      <Route path='/support' component={SupportPage}/> 
      <Route path='/contact' component={ContactPage}/>
      {/* <Route path='/contact' component={ModalDialog}/>  */}
-     <Route path='/nailer-new' component={NailerPageNew}/>
-     <Route path='/nailer-social' component={NailerPageSocial}/>
-<<<<<<< HEAD
+     {/* <Route path='/nailer-new' component={NailerPageNew}/> */}
      <Route path='/profileuser' component={ProfileUser}/>
 =======
-     <Route path='/SignIn' component={FormSignInComponent}/>
+     {/* <Route path='/SignIn' component={FormSignInComponent}/>
      <Route path='/Banggiadichvu' component={Bangiadichvu}/>
->>>>>>> 503a9f7f74c5d4cf2e9fdc8b182ceade39484d71
+>>>>>>> 503a9f7f74c5d4cf2e9fdc8b182ceade39484d71 */}
    </Switch>
  </main>
 )
