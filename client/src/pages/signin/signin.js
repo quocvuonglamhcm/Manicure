a269164af293c322f516f3ac38e4be7b4dc251e0
Nibbs <superangel.lucky@gmail.com>
2019-03-25 22:23:40 +0700

    Anh Long oi! I give up

--- client/src/pages/signin/signin.js
+++ client/src/pages/signin/signin.js
@@ -2,13 +2,40 @@ import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import { Row, Col, Button } from 'react-bootstrap';
 import './signin.css';
-class SignIn extends Component  {
+import IconZalo from '../../assets/images/Logo_Zalo.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faGoogle } from '@fortawesome/free-brands-svg-icons'
+
+class SignIn extends Component {
     render() {
+        let ic_facebook = <FontAwesomeIcon className="icFacebook " icon={faFacebookF} />
+        let ic_google = <FontAwesomeIcon className="icGoogle " icon={faGoogle} />
         return (
             <div className="SignInComponent--Nailer">
                 <Row>
                     <Col>
                         <div className="text-center">đăng nhập bằng</div>
+                    </Col>
+                </Row>
+                <Row className="icon">
+                    <Col sm={4}>
+                    <div className="box-Google">
+                            <Col sm={1}>{ic_google}</Col>
+                            
+                        </div>
+                        
+                    </Col>
+                    <Col sm={4}>
+                        <div className="box-Facebook">
+                            <Col sm={1}>{ic_facebook}</Col>
+                            
+                        </div>
+                    </Col>
+                    <Col sm={4}>
+                    <div className="box-Zalo">
+                            <Col sm={1}><div className="icZalo"></div></Col>
+                            
+                        </div>
                     </Col>
                 </Row>
                 <Row className='justify-content-center'>
@@ -18,7 +45,7 @@ class SignIn extends Component  {
                                 <Form.Label column sm="4">
                                     Số điện thoại
                                 </Form.Label>
-                                <Col sm="8">
+                                <Col sm={8}>
                                     <Form.Control type="text" placeholder="Nhập số điện thoại" />
                                 </Col>
                             </Form.Group>
@@ -28,9 +55,9 @@ class SignIn extends Component  {
                                     Mật khẩu
                                 </Form.Label>
                                 <Col sm="4">
-                                    <Form.Control type="password" placeholder="Nhập Password" />
+                                    <Form.Control type="password" placeholder="Nhập mật khẩu" />
                                 </Col>
-                                <Col sm='4'>
+                                <Col sm={4}>
                                     <Button className="qmk">Quên mật khẩu</Button>
                                 </Col>
                             </Form.Group>
