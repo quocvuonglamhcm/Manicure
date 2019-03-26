0df6e042829107f2db71a9ebb94bbea25c289640
cloud608 <bebu608@gmail.com>
2019-03-26 20:45:55 +0700

    bang gia

--- client/src/pages/banggiadichvu/banggiadichvu.js
+++ client/src/pages/banggiadichvu/banggiadichvu.js
@@ -1,90 +1,126 @@
 import React, { Component } from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Button from 'react-bootstrap/Button';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import Form from 'react-bootstrap/Form';
+import './banggiadichvu.css';
+//import datlich from '../../assets/images/plus-circle-solid.svg';
+//import datlich1 from '../../assets/images/minus-circle-solid.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
+import { Button, Row, Col, Container, InputGroup, FormControl } from 'react-bootstrap/'
 
 class BangGiaDichVu extends Component {
-  state = {
-    items: ['1', '5','56', '555']
-  }
-  componentDidMount() {
-    console.log(this.state.items)
-    this.state.items.map(function(item, i){
-      console.log(item)
-    })
-  }
-  render() {
 
+  render() {
+    let ic_plus = <FontAwesomeIcon className="ic_plus " icon={faPlusCircle} />
+    let ic_minus = <FontAwesomeIcon className="ic_minus" icon={faMinusCircle} />
     return (
       <div className="BangGiaDichVu">
-        <h1>Bang Gia Dich Vu</h1>
-        <Col><button oneclick><h1>+</h1></button></Col>        
-        {this.state.items.map((item, i) => <li>{item}</li>)}
-        <Container>
-          <Row>
-            <Col>Loai Hinh</Col>
-            <Col>Gia Tham Khao</Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form>
-                <Form.Group size="sm" type="text" placeholder="Small text" controlId="exampleForm.ControlSelect1">
-                  <Form.Control size="sm" type="text" placeholder="Small text" as="select">
-                    <option>Tron goi tay hoac chan</option>
-                    <option>Cham soc mong tay hoac mong chan</option>
-                    <option>Dap bot</option>
-                    <option>Dap mong gel</option>
-                    <option>Ve trang tri</option>
-                    <option>Dinh da</option>
-                  </Form.Control>
-                </Form.Group>
-              </Form>
-            </Col>
-            <Col><h4>Tu</h4></Col>
-            <Col>
-              <div>
-              <Form.Control size="sm" type="text" placeholder="gia dich vu" />
-              </div>
-            </Col>
-            <Col><h4>Den</h4></Col>
-            <Col>
-              <Form.Control size="sm" type="text" placeholder="gia dich vu" />
-            </Col>
-            <Col><h4>VND</h4></Col>
-            
-            <Col><button oneclick>-</button></Col>
-          </Row>
-          <Row>
-            <Col>
-              <div>
-                <InputGroup className="mb-3">
-                  <InputGroup.Prepend>
-                    <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-                  </InputGroup.Prepend>
-                  Toi dong y voi cac dieu khoan hoat dong va chinh sach bao mat cua nail bartner
-                </InputGroup>
-               
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <div>
-                <ButtonToolbar>
-                  <Button variant="primary" size="lg">
-                    Dang ky
-                </Button>
+        
+          <div><h4 className="text-title">Bang Gia Dich Vu</h4></div>
+          <Container className="container">
+
+            <div className="text-title1">Loai Hinh</div>
+            <div className="text-title2">Gia Tham Khao VND</div>
+
+
+            <Form>
+
+             
+               <Row>
+                <Col className="col-canh" sm={4}>
+                  <Form.Group size="sm" type="text" placeholder="Small text" controlId="exampleForm.ControlSelect1">
+                    <Form.Control size="sm" type="text" placeholder="Small text" as="select">
+                      <option>Tron goi tay hoac chan</option>
+                      <option>Cham soc mong tay hoac mong chan</option>
+                      <option>Dap bot</option>
+                      <option>Dap mong gel</option>
+                      <option>Ve trang tri</option>
+                      <option>Dinh da</option>
+                    </Form.Control>
+                  </Form.Group>
+
+                </Col>
+                <Col ><h4 className="gia">Tu</h4></Col>
+                <Col className="col-nhap-gia" sm={2}>
+                  <Form.Control size="sm" type="text" placeholder="nhap gia" />
+                </Col>
+                <Col sm={2}><h4 className="gia1">~</h4></Col>
+                <Col className="col-nhap-gia1" sm={2}>
+                  <Form.Control size="sm" type="text" placeholder="nhap gia" />
+                </Col>
+                <Col><h4 className="gia">VND</h4></Col>
+                <Col><Button size="sm" variant="suscess" className="button1">{ic_plus}</Button> </Col>
+               </Row>
+               <Row>
+                <Col className="col-canh" sm={4}>
+                  <Form.Group size="sm" type="text" placeholder="Small text" controlId="exampleForm.ControlSelect1">
+                    <Form.Control size="sm" type="text" placeholder="Small text" as="select">
+                      <option>Tron goi tay hoac chan</option>
+                      <option>Cham soc mong tay hoac mong chan</option>
+                      <option>Dap bot</option>
+                      <option>Dap mong gel</option>
+                      <option>Ve trang tri</option>
+                      <option>Dinh da</option>
+                    </Form.Control>
+                  </Form.Group>
+
+                </Col>
+                <Col ><h4 className="gia">Tu</h4></Col>
+                <Col className="col-nhap-gia" sm={2}>
+                  <Form.Control size="sm" type="text" placeholder="nhap gia" />
+                </Col>
+                <Col sm={2}><h4 className="gia1">~</h4></Col>
+                <Col className="col-nhap-gia1" sm={2}>
+                  <Form.Control size="sm" type="text" placeholder="nhap gia" />
+                </Col>
+                <Col><h4 className="gia">VND</h4></Col>
+                
+                <Col><Button size="sm" variant="suscess" className="button1">{ic_plus}</Button>
+                <Button size ="sm" variant="suscess" className="button2">{ic_minus}</Button>
+                </Col>                
+               </Row>
+               <Row>
+                <Col className="col-canh" sm={4}>
+                  <Form.Group size="sm" type="text" placeholder="Small text" controlId="exampleForm.ControlSelect1">
+                    <Form.Control size="sm" type="text" placeholder="Small text" as="select">
+                      <option>Tron goi tay hoac chan</option>
+                      <option>Cham soc mong tay hoac mong chan</option>
+                      <option>Dap bot</option>
+                      <option>Dap mong gel</option>
+                      <option>Ve trang tri</option>
+                      <option>Dinh da</option>
+                    </Form.Control>
+                  </Form.Group>
+
+                </Col>
+                <Col ><h4 className="gia">Tu</h4></Col>
+                <Col className="col-nhap-gia" sm={2}>
+                  <Form.Control size="sm" type="text" placeholder="nhap gia" />
+                </Col>
+                <Col sm={2}><h4 className="gia1">~</h4></Col>
+                <Col className="col-nhap-gia1" sm={2}>
+                  <Form.Control size="sm" type="text" placeholder="nhap gia" />
+                </Col>
+                <Col><h4 className="gia">VND</h4></Col>
+                
+                <Col><Button size="sm" variant="suscess" className="button1">{ic_plus}</Button>
+                <Button size ="sm" variant="suscess" className="button2">{ic_minus}</Button>
+                </Col>                
+               </Row>
+            </Form>
+            <Row>
+
+              <div className="button-dang-ky1">
+                <ButtonToolbar className="button-dang-ky">
+                  <Button variant size="lg" className="button-dang-ky">Hoan Thanh</Button>
                 </ButtonToolbar>
               </div>
-            </Col>
 
-          </Row>
-        </Container>;
-      </div>
+
+            </Row>
+          </Container>
+        
+      </div >
     );
   }
 }
