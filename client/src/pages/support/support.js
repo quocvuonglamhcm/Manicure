fbe0282c93c48909ce51c9b34835976a17c94f0f
Aiz <kaage.azusa@gmail.com>
2019-03-26 21:24:45 +0700

    'support-change'

--- client/src/pages/support/support.js
+++ client/src/pages/support/support.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { Button, Row, Col, Container, InputGroup, FormControl } from 'react-bootstrap/'
 import './support.css';
+import Card from 'react-bootstrap/Card'
 
 import datlich from '../../assets/images/ic_datlich.svg';
 import kythuat from '../../assets/images/ic_.svg';
@@ -10,14 +11,32 @@ import taikhoan from '../../assets/images/ic_taikhoan.svg';
 import tho from '../../assets/images/ic_tho.svg';
 
 class SupportPage extends Component {
-	state = {
-		name: [
-			'đặt lịch',
-			'kỹ thuật',
-			'tài khoản',
-			'từ người thợ'
-		]
-	}
+	// state = {
+	// 	name: [
+	// 		{
+	// 			name: 'Đặt lịch',
+	// 			image: { datlich }
+	// 		},
+	// 		{
+	// 			name: 'Kỹ thuật',
+	// 			image: { kythuat }
+	// 		},
+	// 		{
+	// 			name: 'Tài khoản',
+	// 			image: { taikhoan }
+	// 		},
+	// 		{
+	// 			name: 'Từ người thợ',
+	// 			image: { tho }
+	// 		}
+	// 	]
+	// 	// Image: [
+	// 	// 	{datlich},
+	// 	// 	{kythuat},
+	// 	// 	{taikhoan},
+	// 	// 	{tho}
+	// 	// ]
+	// }
 	render() {
 		let ic_search = <FontAwesomeIcon className="ic-search " icon={faSearch} />
 		return (
@@ -40,13 +59,43 @@ class SupportPage extends Component {
 					</Col>
 				</Row>
 				<Row>
-					<Item data={this.state.name} />
-				
+					<Col sm={3}>
+						<Card>
+							<Card.Img variant="top" src={datlich}/>
+							<Card.Body>
+								<Card.Title>Đặt lịch</Card.Title>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col sm={3}>
+						<Card>
+							<Card.Img variant="top" src={kythuat}/>
+							<Card.Body>
+								<Card.Title>Kỹ thuật</Card.Title>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col sm={3}>
+						<Card>
+							<Card.Img variant="top" src={taikhoan}/>
+							<Card.Body>
+								<Card.Title>Tài khoản</Card.Title>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col sm={3}>
+						<Card>
+							<Card.Img variant="top" src={tho}/>
+							<Card.Body>
+								<Card.Title>Từ người thợ</Card.Title>
+							</Card.Body>
+						</Card>
+					</Col>
 				</Row>
 				<Row className="m-4">
 					<Col>
-						<h3 className="mb-2">Liên hệ với chúng tôi!</h3>
-						<Button variant="danger" className='p-3'>Trò chuyện ngay</Button>
+						<div className="mb-2 btn-slo">Liên hệ với chúng tôi!</div>
+						<Button variant="danger" className='p-3 contact-btn'>Trò chuyện ngay</Button>
 					</Col>
 				</Row>
 			</Container >
@@ -55,25 +104,25 @@ class SupportPage extends Component {
 }
 
 
-const Item = (props) => {
-	let data = props.data;
-	let element = data.map((item, index) => {
-		return (
-			<Col key={index}>
-				<div className='box-image'>
-					<div className="image">
-						<img src={datlich} />
-						<div className="mn_text">{item}</div>
-					</div>
-				</div>
-			</Col>
-		)
-	})
-	console.log(element)
-	return (
-		element
+// const Item = (props) => {
+// 	let data = props.data;
+// 	let element = data.map((item, index) => {
+// 		return (
+// 			<Col key={index}>
+// 				<div className='box-image'>
+// 					<div className="image">
+// 					{/* <img src={datlich} /> */}
+// 						<div className="mn_text">{item}</div>
+// 					</div>
+// 				</div>
+// 			</Col>
+// 		)
+// 	})
+// 	console.log(element)
+// 	return (
+// 		element
 
-	)
-	
-}
+// 	)
+
+// }
 export default SupportPage;
