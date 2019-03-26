b39e9ef91c468a55b934c30ea376459f678f998c
Minh Trí <MinhTri@TmTzMinhTri.local>
2019-03-27 01:05:57 +0700

    add module nodemon

--- server/apis/welcome.js
+++ server/apis/welcome.js
@@ -3,7 +3,7 @@ const router  = express.Router()
 
 router.get('/api/welcome', (req, res) => {
   console.log('welcom');
-  res.send('Welcome Nails');
+  res.send('Welcome Nails111');
   res.end();
 })
 
