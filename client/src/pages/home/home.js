f28474cc79f114beb3d92b2621aa389c589dc792
Nibbs <superangel.lucky@gmail.com>
2019-03-27 00:57:32 +0700

    Thieu Nhan

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import './home.css';
-import Banner from '../../components/Banner/banner'
-import ProfileUserdetail from '../../components/profileuserdetail/profileuserdetail';
+import Banner from '../../components/Banner/banner';
+
 const HomePage = () => (
 	<div>
 		<Banner />
 		<div className='container'>
 			<div className='register-title'>
 			</div>
-			<div className='body-loi-ich'>
-				<ProfileUserdetail />
-			</div>
 		</div>
+		
 	</div>
 )
 
