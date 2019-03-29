5a36eae9389621fb8d41987ad4c0a2865af5c854
quocvuonglamhcm <quocvuonglamhcm@gmail.com>
2019-03-30 00:25:28 +0700

    them chinh sach bao mat

--- client/src/pages/upproducts/upproducts.js
+++ client/src/pages/upproducts/upproducts.js
@@ -8,14 +8,14 @@ class upProductsPage extends Component {
       <div className='uploadProduct'>
         <Container>
           <Row className="mt-5">
-            <Col sm={3} md={3} sx={3}></Col>
-            <Col sm={6} md={6} sx={6} className="callText textSize1">
+            <Col md={3}></Col>
+            <Col md={6} className="callText textSize1">
               Đăng sản phẩm độc đáo theo phong cách riêng của bạn
                 </Col>
           </Row>
           <Row className='mt-5'>
-            <Col sm={1} md={1} sx={1}></Col>
-            <Col sm={4} md={4} sx={4}>
+            <Col md={1}></Col>
+            <Col md={4}>
               <form>
                 <div className="form-group">
                   <input class="form-control" type="text" placeholder="Tên sản phẩm" />
@@ -40,13 +40,13 @@ class upProductsPage extends Component {
                 </div>
               </form>
             </Col>
-            <Col sm={6} md={6} sx={6}>
+            <Col md={6}>
               <div className="pictureView">Xem trước ảnh tác phẩm</div>
             </Col>
           </Row>
           <Row>
-            <Col sm={5} md={5} sx={5}></Col>
-            <Col sm={2} md={2} sx={2}>
+            <Col md={5}></Col>
+            <Col md={2}>
               <input className="btn btn-primary saveButton" type="submit" value="Đăng ảnh" />
             </Col>
           </Row>
