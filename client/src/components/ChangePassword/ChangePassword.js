b25458c2a1c71770e664dc193320bf3218300b77
Nibbs <superangel.lucky@gmail.com>
2019-04-04 21:20:55 +0700

    Co len Trong Anh

--- client/src/components/ChangePassword/ChangePassword.js
+++ client/src/components/ChangePassword/ChangePassword.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
+import './ChangePassword.css';
 
 export default class TronGoi extends React.Component {
   render() {
     return (
       <Container>
-        <Row className='avatar'>
+        <Row>
+          <div className='avatar'>
+          </div>
         </Row>
-        <Row className='Title'>
+        <Row>
+          <div className='Title'>
+            Đổi mật khẩu
+          </div>
+        </Row>
+        <Row>
+          <div className="Content-Password">
+            <Row>
+              <Col sm={4} md={4} sx={4} className="Left-Password">
+                Mật khẩu
+            </Col>
+              <Col sm={8} md={8} sx={8}>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={4} md={4} sx={4} className="Left-Password">
+                Mật khẩu mới
+            </Col>
+              <Col sm={8} md={8} sx={8}>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={4} md={4} sx={4} className="Left-Password">
+                Xác nhận mật kểu mới
+            </Col>
+              <Col sm={8} md={8} sx={8}>
+              </Col>
+            </Row>
+          </div>
         </Row>
 
       </Container>
-      )
-    }
-  
+    )
   }
+
+}
