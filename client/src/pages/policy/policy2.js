6c1267c46854389df255e37ea52121453ab33459
quocvuonglamhcm <quocvuonglamhcm@gmail.com>
2019-04-06 00:22:42 +0700

    edit policy

--- client/src/pages/policy/policy2.js
+++ client/src/pages/policy/policy2.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './policy.css';
     
-class policy1Page extends Component {
+class policy2Page extends Component {
     render() {
         return (
             <div className="dataSecurity">
                 <Container>
                     <Row className="mt-5">
-                        <Col md={4}></Col>
-                        <Col md={4} className="title">CHÍNH SÁCH BẢO MẬT</Col>
+                        <Col md={3}></Col>
+                        <Col md={6} className="title">MỤC ĐÍCH SỬ DỤNG THÔNG TIN</Col>
                     </Row>
                     <Row className="mt-4 mb-5">
                         <Col md={4} className="leftContent">
-                            <div className="tag">Thông tin chúng tôi thu thập</div>
-                            <div className="tag">Mục đích sử dụng thông tin</div>
-                            <div className="tag">Mục đích chia sẻ thông tin</div>
-                            <div className="tag">Thông tin chi tiết</div>
-                            <div className="tag">Cộng đồng hỗ trợ</div>
-                            <div className="tag">Những sự lựa chọn của bạn</div>
-                            <div className="tag">Quyền dữ liệu</div>
+                            <div className="tag"><Link to={'/policy1'}>Thông tin chúng tôi thu thập</Link></div>
+                            <div className="tag"><Link to={'/policy2'}>Mục đích sử dụng thông tin</Link></div>
+                            <div className="tag"><Link to={'/policy3'}>Mục đích chia sẻ thông tin</Link></div>
+                            <div className="tag"><Link to={'/policy4'}>Thông tin chi tiết</Link></div>
+                            <div className="tag"><Link to={'/policy5'}>Cộng đồng hỗ trợ</Link></div>
+                            <div className="tag"><Link to={'/policy6'}>Những sự lựa chọn của bạn</Link></div>
+                            <div className="tag"><Link to={'/policy7'}>Quyền dữ liệu</Link></div>
                         </Col>
                         <Col md={8}>
                             <div className="contentBox">
@@ -81,4 +82,4 @@ class policy1Page extends Component {
     }
 }
 
-export default policy1Page;
+export default policy2Page;
