461b6a99e1040f0001011ab9bccee2561916f37a
Nibbs <superangel.lucky@gmail.com>
2019-04-11 21:32:10 +0700

    Hen moi nguoi 18h ngay 13/4/2019

---
+++ client/src/pages/ProfileNailer/UpdateName.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+class UpdateImage extends Component {
+  render() {
+    const ic_star = <FontAwesomeIcon className="fontSize25 yellow " icon={faStar} />
+    return (
+      <div>
+        <div className='nailer-name whiteColor mt-2'>Huynh Thi B</div>
+        <div className='nailer-rate mt-2 mb-2'>{ic_star}{ic_star}{ic_star}{ic_star}{ic_star}</div>
+      </div>
+    );
+  }
+}
+export default UpdateImage;      
